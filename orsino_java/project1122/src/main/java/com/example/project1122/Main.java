0e3a4c63e9998c2b48e31d5939d09ad462dc430c
LAPTOP-AKLUQKTK\a4065 <02love216@gmail.com>
2025-12-01 14:13:17 +0800

    第14堂 伺服器給json格式的client回傳結果

--- orsino_java/project1122/src/main/java/com/example/project1122/Main.java
+++ orsino_java/project1122/src/main/java/com/example/project1122/Main.java
@@ -1,9 +1,10 @@
 package com.example.project1122;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.json.*;
 
 
 @RestController
@@ -27,10 +28,24 @@ public class Main {
         return "Get Form submitted.";
     }
     //post送資料：繳交的物件msg from Message.java
+    // 代收人員@RequestBody辨別物件msg
     @PostMapping("/submit")
     public String string(@RequestBody Message msg) {
 
-        //代收人員@RequestBody辨別物件mgs，模擬json字串包含key:value欄位，name與text兩個字串內容
-        return "{\"message\":\"收到來自 " + msg.name + " 的訊息: " + msg.text+ "\"}";
+        //方法一：模擬json字串包含key:value欄位，name與text兩個字串內容
+        // return "{\"message\":\"收到來自 " + msg.name + " 的訊息: " + msg.text+ "\"}";
+
+        //方法二：利用JSONObject建立"回傳的訊息物件"並轉換成json字串 執行回傳
+        JSONObject json = new JSONObject();
+        json.put("message", "收到來自 " + msg.name + " 的訊息: " + msg.text);
+        json.put("message2", "!!!");
+        json.put("status2", new JSONObject().put("name", msg.name).put("text", msg.text)); 
+        json.put("weatherElements", new JSONArray()
+                .put(new JSONObject().put("地點", "台北市").put("天氣", "雨天"))
+                .put(new JSONObject().put("地點", "新竹市").put("天氣", "雨天"))
+                .put(new JSONObject().put("地點", "高雄市").put("天氣", "雨天"))
+        );
+           //建立key:status2, value:另一個json物件{} 包含name與text欄位
+        return json.toString();
     }
 }
